0da34ae58d001fb7d2fc7511ac05d852c8edb7d6
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-08 14:28:40 -0300

    {{ Inserindo print django }}

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', status=201)
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz',
+    })
 
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'me-apague/temp.html')
 
 
 def sobre(request):
