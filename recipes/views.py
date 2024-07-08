839c7aeddfaea88ddfdcc9139dce4a7d0de48acd
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-08 15:16:42 -0300

    Removido sobre e contato

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Luiz',
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
