0e08cba81367d1e4c73b7ec422f3494b012cf04b
Fabrício Adriel Coelho Freire <fabricioadriel@yahoo.com.br>
2024-07-11 12:27:57 -0300

    Correção do erro no such table

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
-
 class CategoryAdmin(admin.ModelAdmin):
-    ...
-
+    ... 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
 
-
 admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
-
-
+    
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servings = models.IntegerField
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
