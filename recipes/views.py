7fb78d3010aa1f2617024f06b9aafa009fed4d44
Fabrício Adriel Coelho Freire <fabricioadriel@yahoo.com.br>
2024-07-12 15:20:53 -0300

    Atualização de estudo

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
