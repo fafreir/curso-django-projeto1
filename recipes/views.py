ce0f34dc7b607c4a4dbda8c1fccd3c1fe6ab2928
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-13 19:37:43 -0300

    URL not found, utilizando Http404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -16,7 +16,7 @@ def category(request, category_id):
         category__id=category_id, is_published=True).order_by('-id')
 
     if not recipes:
-        return HttpResponse(content='Not found', status=404)
+        raise Http404('Page not found')
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
